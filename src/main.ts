1bb833aa30e6ea45d890913f09a2d6e7c6a97169
joseph <chukwuj40@gmail.com>
2024-05-05 17:18:27 +0100

    cor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,44 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // setup swagger docs
+  const swagConfig = new DocumentBuilder()
+    .setTitle('Naiya Swagger Documentation')
+    .setDescription('Naiya Swagger Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'JWT',
+    )
+    .build();
+  SwaggerModule.setup(
+    '/swagger',
+    app,
+    SwaggerModule.createDocument(app, swagConfig),
+    {
+      explorer: true,
+      swaggerOptions: {
+        docExpansion: 'none',
+        filter: true,
+        showRequestHeaders: true,
+      },
+    },
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
+
 bootstrap();
